0a5b0a48dcf82b72c25af9a9cfe428f3a87b7185
Paul Florian Bruchhaeuser <paul.bruchhaeuser@gmail.com>
2025-12-28 17:46:43 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Grid from "./Grid";
+import Imprint from "./Imprint";
+import Contact from "./Contact";
+
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedXml, setSelectedXml] = useState(null);
+  const [showImprint, setShowImprint] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+
+  const toggleArticle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const openArticleWithXml = (xml) => {
+    setSelectedXml(xml);
+    setIsOpen(true);
+  };
+
+  const openImprint = () => {
+    setShowImprint(true);
+  };
+
+  const closeImprint = () => {
+    setShowImprint(false);
+  };
+
+  const openContact = () => {
+    setShowContact(true);
+  };
+
+  const closeContact = () => {
+    setShowContact(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main_content">
+        <Header onImprintClick={openImprint} onContactClick={openContact} />
+
+        {showImprint ? (
+          <Imprint onClose={closeImprint} />
+        ) : showContact ? (
+          <Contact onClose={closeContact} />
+        ) : (
+          <Grid
+            onItemClick={openArticleWithXml}
+            articleIsOpen={isOpen}
+            articleOnClose={toggleArticle}
+            articleXml={selectedXml}
+          />
+        )}
+      </div>
     </div>
   );
 }
